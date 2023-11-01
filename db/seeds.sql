9c0ca7415190287668fb260bd2f6552082616645
Sonja <sonjaljatkovic@gmail.com>
2023-11-01 19:18:09 -0400

    added role seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,8 +7,15 @@ VALUES  ("Communications"),
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES()
+VALUES  ("Executive Officer", 85000, 1),
+        ("Assistant Treasurer", 55000, 2),
+        ("General Manager of HR", 80000, 3),
+        ("Superintendent", 65000, 4),
+        ("Program Supervisor", 70000, 5);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES()
+VALUES("Spencer", "Reid", )
+("Penelope", "Garcia"),
+("Aaron", "Hotchner"),
+("Jennifer", "Jareau"),

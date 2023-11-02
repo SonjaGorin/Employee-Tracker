b518ef82db3a949dfcb1bb8367ae945c20a05ed3
Sonja <sonjaljatkovic@gmail.com>
2023-11-02 11:51:32 -0400

    added prompt list

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const inquirer = require("inquirer");
 const db = require("./server.js");
 
 const questions = [
-    
+    {
+        type: "list",
+        name: "options",
+        message: "Please choose an option from the following list:",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+    }
 ]

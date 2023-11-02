0f4c7ed77ab3ff31a36f81b2ef51950563872d38
Sonja <sonjaljatkovic@gmail.com>
2023-11-02 14:51:58 -0400

    fixing joins

--- db/query.sql
+++ db/query.sql
@@ -1,12 +1,11 @@
-SELECT * 
+SELECT role.id, role.title, role.salary, department.name
 FROM role
 JOIN department ON role.department_id = department.id;
 
-
-SELECT *
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.department, role.salary
 FROM employee
 JOIN role ON employee.role_id = role.id;
 
-SELECT *
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.department, role.salary, employee.id
 FROM employee
 JOIN role ON employee.manager_id = employee.id;
